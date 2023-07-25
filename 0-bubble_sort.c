14bee52a784106379c7ef0a4641419f888e165a6
jemal1444 <jemal.eyasu@yahoo.com>
2023-07-25 21:04:42 +0300

    This is an ALX Task project on sorting algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * Description on  bubble_sort - sorts an array of integers in ascending order using
+ * @array: array to sort
+ * @size: length of the array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, tmp = 0, n = size;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (n > 0)
+	{
+		for (i = 0; i < (int)size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
+		}
+		n--;
+	}
+}
